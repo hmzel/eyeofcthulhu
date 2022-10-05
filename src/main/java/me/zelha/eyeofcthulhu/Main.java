9efeb33aa60d5409c4e7b5072bf27790ad37ca1c
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-04 19:25:19 -0500

    Initial commit

---
+++ src/main/java/me/zelha/eyeofcthulhu/Main.java
@@ -0,0 +1,15 @@
+package me.zelha.eyeofcthulhu;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        World world = Bukkit.getWorld("zelha");
+
+        
+    }
+}
