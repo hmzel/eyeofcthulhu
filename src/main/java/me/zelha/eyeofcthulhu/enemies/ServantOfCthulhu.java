0e02d299913daf2035af52928b38f0ec172605f8
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-17 19:00:57 -0500

    added basic AI to ServantOfCthulhu

--- src/main/java/me/zelha/eyeofcthulhu/enemies/ServantOfCthulhu.java
+++ src/main/java/me/zelha/eyeofcthulhu/enemies/ServantOfCthulhu.java
@@ -4,12 +4,18 @@ import hm.zelha.particlesfx.particles.ParticleDust;
 import hm.zelha.particlesfx.particles.parents.Particle;
 import hm.zelha.particlesfx.shapers.ParticleLine;
 import hm.zelha.particlesfx.shapers.ParticleSphere;
+import hm.zelha.particlesfx.util.LVMath;
 import hm.zelha.particlesfx.util.LocationSafe;
 import hm.zelha.particlesfx.util.ParticleShapeCompound;
+import me.zelha.eyeofcthulhu.Main;
 import me.zelha.eyeofcthulhu.util.Hitbox;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.World;
+import net.minecraft.server.v1_8_R3.EntityLiving;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import org.bukkit.*;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.entity.*;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -22,13 +28,14 @@ public class ServantOfCthulhu {
     private final ThreadLocalRandom rng = ThreadLocalRandom.current();
     private final ParticleShapeCompound servant = new ParticleShapeCompound();
     private final Hitbox hitbox;
+    private EntityLiving target = null;
 
     public ServantOfCthulhu(Location location) {
         World world = location.getWorld();
         LocationSafe center = new LocationSafe(world, location.getX(), location.getY(), location.getZ());
         Particle tendrilRed = new ParticleDust(Color.RED, 75);
         ParticleSphere body = new ParticleSphere(RED, center, 0.5, 0.5, 0.5, 7, 50);
-        this.hitbox = new Hitbox(servant, 0.75, 1, center, 10, null);
+        this.hitbox = new Hitbox(servant, 1, 1, center, 10, null);
 
         servant.addShape(body);
 
@@ -59,5 +66,97 @@ public class ServantOfCthulhu {
         body.addParticle(WHITE, 29);
         body.addParticle(PURPLE, 34);
         body.addParticle(BLACK, 46);
+
+        startAI();
+    }
+
+    public void startAI() {
+        findTarget();
+
+        new BukkitRunnable() {
+
+            private final ParticleSphere body = (ParticleSphere) servant.getShape(0);
+            private final Location l = body.getCenter().clone();
+            private final Vector vHelper = new Vector(0, 0, 0);
+
+            @Override
+            public void run() {
+                if (!body.isRunning()) {
+                    cancel();
+                    return;
+                }
+
+                if (target == null || !target.valid || !target.isAlive()) {
+                    findTarget();
+                    return;
+                }
+
+                l.zero().add(target.locX, target.locY + 1.5, target.locZ);
+                LVMath.subtractToVector(vHelper, l, body.getCenter());
+                vHelper.normalize().multiply(0.2);
+                servant.move(vHelper);
+                servant.face(l);
+
+                if (!(target instanceof EntityPlayer)) {
+                    for (Entity e : l.getWorld().getNearbyEntities(body.getCenter(), 1, 1, 1)) {
+                        if (e.equals(target.getBukkitEntity())) {
+                            ((LivingEntity) e).damage(hitbox.getDamage(), hitbox.getSlime());
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(Main.getInstance(), 0, 1);
+    }
+
+    private void findTarget() {
+        Location center = ((ParticleSphere) servant.getShape(0)).getCenter();
+        Entity target = null;
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (!center.getWorld().equals(p.getWorld())) continue;
+            if (p.getGameMode() != GameMode.SURVIVAL) continue;
+
+            if (target == null) {
+                target = p;
+
+                continue;
+            }
+
+            if (p.getLocation().distance(center) < target.getLocation().distance(center)) {
+                target = p;
+            }
+        }
+
+        if (target != null && target.getLocation().distance(center) > 25) target = null;
+
+        for (Entity e : center.getWorld().getNearbyEntities(center, 50, 50, 50)) {
+            if (e instanceof Player) continue;
+            if (e instanceof Slime) continue;
+            if (e instanceof Monster) continue;
+            if (!(e instanceof LivingEntity)) continue;
+
+            if (target == null) {
+                target = e;
+
+                continue;
+            }
+
+            if (e.getLocation().distance(center) < target.getLocation().distance(center)) {
+                target = e;
+            }
+        }
+
+        if (target == null) return;
+
+        this.target = (EntityLiving) ((CraftEntity) target).getHandle();
+    }
+
+    public void setTarget(Entity target) {
+        this.target = (EntityLiving) ((CraftEntity) target).getHandle();
     }
 }
+
+
+
+
+
