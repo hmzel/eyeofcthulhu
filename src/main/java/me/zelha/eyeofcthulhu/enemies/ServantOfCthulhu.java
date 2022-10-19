4b1084949046705ce96128ba390f6c833ed5ef68
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-19 00:11:53 -0500

    made ServantOfCthulhu support the previous changes and implemented onHit()

--- src/main/java/me/zelha/eyeofcthulhu/enemies/ServantOfCthulhu.java
+++ src/main/java/me/zelha/eyeofcthulhu/enemies/ServantOfCthulhu.java
@@ -6,39 +6,33 @@ import hm.zelha.particlesfx.shapers.ParticleLine;
 import hm.zelha.particlesfx.shapers.ParticleSphere;
 import hm.zelha.particlesfx.util.LVMath;
 import hm.zelha.particlesfx.util.LocationSafe;
-import hm.zelha.particlesfx.util.ParticleShapeCompound;
 import me.zelha.eyeofcthulhu.Main;
 import me.zelha.eyeofcthulhu.util.Hitbox;
 import net.minecraft.server.v1_8_R3.EntityLiving;
-import org.bukkit.*;
+import org.bukkit.Color;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.*;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-
-public class ServantOfCthulhu {
+public class ServantOfCthulhu extends ParticleEnemy {
 
     private static final Particle WHITE = new ParticleDust(Color.WHITE).setPureColor(true);
     private static final Particle BLACK = new ParticleDust(Color.BLACK);
     private static final Particle RED = new ParticleDust(Color.RED, 85);
     private static final Particle PURPLE = new ParticleDust(Color.PURPLE);
-    private final ThreadLocalRandom rng = ThreadLocalRandom.current();
-    private final ParticleShapeCompound servant = new ParticleShapeCompound();
-    private final Hitbox hitbox;
-    private EntityLiving target = null;
 
     public ServantOfCthulhu(Location location) {
         World world = location.getWorld();
         LocationSafe center = new LocationSafe(world, location.getX(), location.getY(), location.getZ());
         Particle tendrilRed = new ParticleDust(Color.RED, 75);
         ParticleSphere body = new ParticleSphere(RED, center, 0.5, 0.5, 0.5, 7, 50);
-        this.hitbox = new Hitbox(servant, 1, 1, center, 10, null);
+        super.hitbox = new Hitbox(this, center, 1, 1, 10, "Servant of Cthulhu", false);
 
-        servant.addShape(body);
+        model.addShape(body);
 
         for (int i = 0; i < 3; i++) {
             ParticleLine tendril = new ParticleLine(tendrilRed, 4,
@@ -46,7 +40,7 @@ public class ServantOfCthulhu {
                     new LocationSafe(world, center.getX(), center.getY() + 1, center.getZ())
             );
 
-            servant.addShape(tendril);
+            model.addShape(tendril);
             tendril.rotateAroundLocation(center, 20, 120 * i, 0);
             tendril.rotate(20, 120 * i, 0);
             tendril.setMechanic((particle, l, vector) ->
@@ -71,12 +65,22 @@ public class ServantOfCthulhu {
         startAI();
     }
 
-    public void startAI() {
+    @Override
+    public void onHit(Entity attacker) {
+        if (attacker instanceof Player && ((Player) attacker).getGameMode() != GameMode.SURVIVAL) {
+            return;
+        }
+
+        target = (EntityLiving) ((CraftEntity) attacker).getHandle();
+    }
+
+    @Override
+    protected void startAI() {
         findTarget();
 
         new BukkitRunnable() {
 
-            private final ParticleSphere body = (ParticleSphere) servant.getShape(0);
+            private final ParticleSphere body = (ParticleSphere) model.getShape(0);
             private final Location l = body.getCenter().clone();
             private final Vector vHelper = new Vector(0, 0, 0);
 
@@ -95,8 +99,8 @@ public class ServantOfCthulhu {
                 l.zero().add(target.locX, target.locY + (target.length / 2), target.locZ);
                 LVMath.subtractToVector(vHelper, l, body.getCenter());
                 vHelper.normalize().multiply(0.2);
-                servant.move(vHelper);
-                servant.face(l);
+                model.move(vHelper);
+                model.face(l);
 
                 for (Entity e : l.getWorld().getNearbyEntities(body.getCenter(), 1, 1, 1)) {
                     if (e instanceof Player) continue;
@@ -108,52 +112,6 @@ public class ServantOfCthulhu {
                 }
             }
         }.runTaskTimer(Main.getInstance(), 0, 1);
-    }
-
-    private void findTarget() {
-        Location center = ((ParticleSphere) servant.getShape(0)).getCenter();
-        Entity target = null;
-
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            if (!center.getWorld().equals(p.getWorld())) continue;
-            if (p.getGameMode() != GameMode.SURVIVAL) continue;
-
-            if (target == null) {
-                target = p;
-
-                continue;
-            }
-
-            if (p.getLocation().distance(center) < target.getLocation().distance(center)) {
-                target = p;
-            }
-        }
-
-        if (target != null && target.getLocation().distance(center) > 25) target = null;
-
-        if (target == null) {
-            List<Entity> nearbyEntities = (ArrayList<Entity>) center.getWorld().getNearbyEntities(center, 50, 50, 50);
-
-            for (int i = 0; i < nearbyEntities.size(); i++) {
-                Entity e = nearbyEntities.get(rng.nextInt(nearbyEntities.size() - 1));
-
-                if (e instanceof Player) continue;
-                if (e instanceof Slime) continue;
-                if (e instanceof Monster) continue;
-                if (!(e instanceof LivingEntity)) continue;
-
-                target = e;
-                break;
-            }
-        }
-
-        if (target == null) return;
-
-        this.target = (EntityLiving) ((CraftEntity) target).getHandle();
-    }
-
-    public void setTarget(Entity target) {
-        this.target = (EntityLiving) ((CraftEntity) target).getHandle();
     }
 }
 
