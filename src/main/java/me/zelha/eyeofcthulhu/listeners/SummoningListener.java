cee4dbe7d7ca0ddbcfd03f9a5ad0f8962bcb4006
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-27 23:23:33 -0500

    now uses the right event and works with minecart chests

--- src/main/java/me/zelha/eyeofcthulhu/listeners/SummoningListener.java
+++ src/main/java/me/zelha/eyeofcthulhu/listeners/SummoningListener.java
@@ -4,9 +4,13 @@ import me.zelha.eyeofcthulhu.Main;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.minecart.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkPopulateEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -16,23 +20,46 @@ public class SummoningListener implements Listener {
     private final ThreadLocalRandom rng = ThreadLocalRandom.current();
 
     @EventHandler
-    public void onChestLoad(ChunkLoadEvent e) {
-        if (!e.isNewChunk()) return;
-
+    public void onChestLoad(ChunkPopulateEvent e) {
         for (BlockState block : e.getChunk().getTileEntities()) {
             if (block.getType() != Material.CHEST) continue;
-            if (rng.nextInt(5) != 1) continue;
 
-            Chest chest = (Chest) block.getBlock();
+            addSummoningItem((Chest) block);
+        }
 
-            for (int i = 0; i < chest.getInventory().getSize(); i++) {
-                ItemStack item = chest.getInventory().getItem(i);
+        for (Entity entity : e.getChunk().getEntities()) {
+            if (!(entity instanceof StorageMinecart)) continue;
 
-                if (item == null || item.getType() == Material.AIR) {
-                    chest.getInventory().setItem(i, Main.getSummonItem());
-                    break;
-                }
+            addSummoningItem((StorageMinecart) entity);
+        }
+    }
+
+    private void addSummoningItem(InventoryHolder invHolder) {
+        if (rng.nextInt(5) != 1) return;
+
+        Inventory inv = invHolder.getInventory();
+
+        for (int i = 0; i < inv.getSize(); i++) {
+            ItemStack item = inv.getItem(i);
+
+            if (item == null || item.getType() == Material.AIR) {
+                inv.setItem(i, Main.getSummonItem());
+                break;
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
